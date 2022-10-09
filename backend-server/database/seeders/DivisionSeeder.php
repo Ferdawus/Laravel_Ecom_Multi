35f9b44b7e0c47b4887cdd792877b1b6e5b0e6bc
ferdawus <ferdawusa@gmail.com>
2022-10-09 09:17:31 +0600

    first commit

--- backend-server/database/seeders/DivisionSeeder.php
+++ backend-server/database/seeders/DivisionSeeder.php
@@ -19,42 +19,50 @@ class DivisionSeeder extends Seeder
             [
                 "id"=> 1,
                 "name"=> "Dhaka",
-                "bn_name"=> "ঢাকা"
+                "bn_name"=> "ঢাকা",
+                "charge" => 60,
             ],
             [
                 "id" => 2,
                 "name"=> "Chittagong",
                 "bn_name"=> "চট্টগ্রাম",
+                "charge" => 120,
             ],
             [
                 "id"=> 3,
                 "name"=> "Rajshahi",
-                "bn_name"=> "রাজশাহী"
+                "bn_name"=> "রাজশাহী",
+                "charge" => 120,
             ],
             [
                 "id"=> 4,
                 "name"=> "Khulna",
-                "bn_name"=>"খুলনা"
+                "bn_name"=>"খুলনা",
+                "charge" => 120,
                 ],
             [
                 "id" =>5,
                 "name"=> "Barisal",
-                "bn_name"=> "বরিশাল"
+                "bn_name"=> "বরিশাল",
+                "charge" => 120,
             ],
             [
                 "id"=> 6,
                 "name"=> "Rangpur",
-                "bn_name"=>"রংপুর"
+                "bn_name"=>"রংপুর",
+                "charge" => 120,
             ],
             [
                 "id"=> 7,
                 "name"=> "Sylhet",
-                "bn_name"=> "সিলেট"
+                "bn_name"=> "সিলেট",
+                "charge" => 120,
             ],
             [
                 "id"=> 8,
                 "name"=> "Mymensingh",
-                "bn_name"=> "ময়মনসিংহ"
+                "bn_name"=> "ময়মনসিংহ",
+                "charge" => 120,
             ]
        ]);
     }
